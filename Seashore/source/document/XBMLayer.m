16be49ad1e47a55ac28e8e4d7620119b5da71dcc
robaho <rengels@ix.netcom.com>
2018-12-22 00:31:47 -0600

    minor warning cleanup

--- Seashore/source/document/XBMLayer.m
+++ Seashore/source/document/XBMLayer.m
@@ -42,7 +42,7 @@
 		do {
 			i++;
 			string[i] = fgetc(file);
-		} while ((i < 8) && (string[i] >= '0' && string[i] <= '9' || string[i] >= 'a' && string[i] <= 'f' || string[i] >= 'A' && string[i] <= 'F' || string[i] == 'x') && !(ferror(file) || feof(file)));
+        } while ((i < 8) && ((string[i] >= '0' && string[i] <= '9') || (string[i] >= 'a' && string[i] <= 'f') || (string[i] >= 'A' && string[i] <= 'F') || string[i] == 'x') && !(ferror(file) || feof(file)));
 		
 		// Fail if something went wrong
 		if (ferror(file) || feof(file)) {
