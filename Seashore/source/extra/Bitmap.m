3303ab2e3b30a621a0a5e5b30cea9b802d1cc2d9
robaho <rengels@ix.netcom.com>
2019-04-28 19:28:21 -0500

    fix issue #47, do not rely on grayscale with alpha support in OSX version

--- Seashore/source/extra/Bitmap.m
+++ Seashore/source/extra/Bitmap.m
@@ -1,25 +1,31 @@
 #include "Bitmap.h"
 #import "bitstring.h"
 
+
+void convertRGBA2GrayA(unsigned char *dbitmap, unsigned char *ibitmap, int width, int height)
+{
+    int i, ispp=4;
+    
+    for (i = 0; i < width * height; i++) {
+        dbitmap[i * 2] = ((int)ibitmap[i * ispp] + (int)ibitmap[i * ispp + 1] + (int)ibitmap[i * ispp + 2]) / 3;
+        if (ispp == 4) dbitmap[i * 2 + 1] = ibitmap[i * 4 + 3];
+    }
+}
+
 /*
  convert NSImageRep to a format Seashore can work with, which is RGBA, or GrayA. If spp is 4, then RGBA, if 2, the GrayA
  */
 unsigned char *convertImageRep(NSImageRep *imageRep,int spp) {
     
-    NSColorSpaceName csname = MyRGBSpace;
-    if (spp==2) {
-        csname = MyGraySpace;
-    }
-    
     int width = (int)[imageRep pixelsWide];
     int height = (int)[imageRep pixelsHigh];
     
-    unsigned char *buffer = calloc(width*height*spp,sizeof(unsigned char));
+    unsigned char *buffer = calloc(width*height*4,sizeof(unsigned char));
     
     NSBitmapImageRep *bitmapWhoseFormatIKnow = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:&buffer pixelsWide:width pixelsHigh:height
-                                                                                    bitsPerSample:8 samplesPerPixel:spp hasAlpha:YES isPlanar:NO
-                                                                                   colorSpaceName:csname bytesPerRow:width*spp
-                                                                                     bitsPerPixel:8*spp];
+                                                                                    bitsPerSample:8 samplesPerPixel:4 hasAlpha:YES isPlanar:NO
+                                                                                   colorSpaceName:MyRGBSpace bytesPerRow:width*4
+                                                                                     bitsPerPixel:8*4];
     
     [bitmapWhoseFormatIKnow setSize:[imageRep size]];
     
@@ -29,7 +35,14 @@ unsigned char *convertImageRep(NSImageRep *imageRep,int spp) {
     [imageRep draw];
     [NSGraphicsContext restoreGraphicsState];
     
-    unpremultiplyBitmap(spp,buffer,buffer,width*height);
+    unpremultiplyBitmap(4,buffer,buffer,width*height);
+    
+    if(spp==2) {
+        unsigned char *dbuffer = calloc(width*height*2,sizeof(unsigned char));
+        convertRGBA2GrayA(dbuffer,buffer,width,height);
+        free(buffer);
+        buffer=dbuffer;
+    }
     
     return buffer;
 }
@@ -170,3 +183,5 @@ inline unsigned char averagedComponentValue(int spp, unsigned char *data, int wi
 		
 	return (total / count);
 }
+
+
