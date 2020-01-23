7e4f5ed3af4f6332b8bb63f6ef58bd54cbfe7eb0
robaho <rengels@ix.netcom.com>
2020-01-22 23:26:15 -0600

    remove unused code

--- Seashore/source/utility/NSBezierPath_Extensions.h
+++
@@ -1,8 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-@interface NSBezierPath(MyExtensions)
-+ (NSBezierPath *)bezierPathWithRect:(NSRect) rect andRadius:(float) radius;
-@end
-
-void NSLogRect(NSRect rect);
