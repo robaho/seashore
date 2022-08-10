de0d29bd4dd451a99c5876262efbcc1ad1302cb6
robaho <rengels@ix.netcom.com>
2022-08-10 18:08:22 -0500

    use 0 instead of nil to match others

--- CoreImage Plug-ins/CISepia/CISepiaClass.m
+++ CoreImage Plug-ins/CISepia/CISepiaClass.m
@@ -4,7 +4,7 @@
 
 - (id)initWithManager:(PluginData *)data
 {
-    return [super initWithManager:data filter:@"CISepiaTone" points:0 properties:kCI_Intensity,nil];
+    return [super initWithManager:data filter:@"CISepiaTone" points:0 properties:kCI_Intensity,0];
 }
 
 @end
